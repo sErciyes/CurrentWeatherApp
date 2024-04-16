9c131fac3702d1fd23752b37f279e8f37b26c18f
sErciyes <151975351+sErciyes@users.noreply.github.com>
2024-04-16 04:21:31 +0300

    Add project files.

---
+++ CurrentWeatherApp/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace CurrentWeatherApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lsbCity = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 322);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // lsbCity
+            // 
+            this.lsbCity.FormattingEnabled = true;
+            this.lsbCity.Items.AddRange(new object[] {
+            "istanbul",
+            "izmir",
+            "ankara",
+            "bolu",
+            "london",
+            "paris",
+            "Moscow"});
+            this.lsbCity.Location = new System.Drawing.Point(18, 24);
+            this.lsbCity.Name = "lsbCity";
+            this.lsbCity.Size = new System.Drawing.Size(146, 199);
+            this.lsbCity.TabIndex = 1;
+            this.lsbCity.SelectedIndexChanged += new System.EventHandler(this.lsbCity_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "tr",
+            "en",
+            "fr",
+            "de",
+            "ru"});
+            this.comboBox1.Location = new System.Drawing.Point(338, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 251);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 41);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 482);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lsbCity);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lsbCity;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CurrentWeatherApp/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace CurrentWeatherApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string city="Ankara";
+        string lang = "tr";
+        string ApiKey = "84a82b559d7a28fa82e633617adaa1b8";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label2.Text= string.Empty;
+            lsbCity.SelectedIndex = 0;comboBox1.SelectedIndex = 0;
+            VeriCek(ApiKey, lsbCity.SelectedItem + "", comboBox1.SelectedItem + "");
+        }
+        private void VeriCek(string Api,string City,string Lang)
+        {
+            string connection = "http://api.openweathermap.org/data/2.5/weather?q=" + City + "&mode=xml&lang=" + Lang + "&units=metric&appid=" + Api;
+            XDocument weather = XDocument.Load(connection);
+            var temp = weather.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            var weatherstatus = weather.Descendants("weather").ElementAt(0).Attribute("value").Value;
+            this.Text = temp + "";
+            label1.Text = "Current Weather Status : " + weatherstatus.ToUpper();
+        }
+
+        private void lsbCity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            VeriCek(ApiKey, lsbCity.SelectedItem + "", comboBox1.SelectedItem + "");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            VeriCek(ApiKey, lsbCity.SelectedItem + "", comboBox1.SelectedItem + "");
+        }
+    }
+}
